b847e1b63a09a10c5724fb4aa73482a6e62318d8
skdksldk <tnwns1485@naver.com>
2023-12-05 12:34:34 +0900

    Replying to Comments

--- client/src/components/commentcard.jsx
+++ client/src/components/commentcard.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/prop-types */
 import { getDay } from "../common/date";
-
+import { useContext, useState } from "react";
+import { UserContext } from "../App";
+import { toast } from "react-hot-toast";
+import CommentField from "./commentfield";
 
 const CommentCard = ({ index, commentData, leftVal = 0 }) => {
 
     let { comment, commented_by: { personal_info: { fullname, username: profile_username , profile_img } }, commentedAt, children, _id } = commentData;
 
+    let { userAuth: { username, access_token } } = useContext(UserContext);
+
+    const [ isReplying, setReplying ] = useState(false);
+    
+    const handleReplyClick = () => {
+        if(!access_token){
+            return toast.error("login first to leave a reply")
+        }
+
+        setReplying(preVal => !preVal);
+    }
+    
     return (
         
         <div className="w-full" style={{ paddingLeft : `${leftVal * 10}px` }}>
@@ -16,6 +31,17 @@ const CommentCard = ({ index, commentData, leftVal = 0 }) => {
                     <p className=" min-w-fit">{getDay(commentedAt)}</p>
                 </div>
                 <p className="font-gelasio text-xl ml-3">{comment}</p>
+
+                <div className="flex gap-5 items-center mt-5">
+                   <button className="underline" onClick={handleReplyClick} >Reply</button>
+                </div>
+                { 
+                    isReplying ? 
+                    <div className="mt-8">
+                        <CommentField action="Reply" index={index} replyingTo={_id} setReplying={setReplying} />
+                    </div>
+                    : "" 
+                }
             </div>
         </div>
 
